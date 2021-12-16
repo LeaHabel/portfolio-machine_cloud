05b914ce1a0b198d82563be4c3167ad3fcdd7715
CarinaSt <carina.steindl@edu.fh-joanneum.at>
2021-12-16 12:25:26 +0100

    IdleState

---
+++ src/IdleState/IdleState.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './IdleState.css';
+
+export function Overview (){
+
+    return (
+        <div className="component-display">
+                <div>
+                    <h1>Have a look above you</h1>
+                </div>
+        </div>
+    );
+
+}
