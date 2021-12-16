d6f9b5293ea825d02ec8a107d2911da1d653aff3
LeaSuLinHabel <lea.habel@yahoo.de>
2021-12-16 12:17:28 +0100

    import images

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,22 +1,26 @@
 import React from "react";
 import './Overview.css';
 
- export function Overview (){
+import Img1 from '../assets/img1.png'
+import Img2 from '../assets/img2.png'
+import Img3 from '../assets/img3.png'
 
-        return (
-            <div className="component-display">
-               <div className="person">
-                   <div id="personName">
+export function Overview() {
+
+    return (
+        <div className="component-display">
+            <div className="person">
+                <div id="personName">
                     <h1>Nicolas Saavedra-Rueda</h1>
-                   <h2>Interaction Design</h2>
-                   </div>
-                   <div className="personImage">
-                       <img src="/assets/img1.png"/>
-                       <img src={'./assets/img1.png'}/>
-                       <img src="/src/assets/img1.png"/>
-                   </div>
-               </div>
+                    <h2>Interaction Design</h2>
+                </div>
+                <div className="personImage">
+                    <img src={Img1} />
+                    <img src={Img2} />
+                    <img src={Img3} />
+                </div>
             </div>
-        );
+        </div>
+    );
 
 }
