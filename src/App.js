c2c591de4a108686f1c19eeb41217417970af77b
ColinSeanKavanagh <colin.sean.kavanagh@gmail.com>
2022-01-01 22:49:20 +0100

    add ThreeJS water // WIP

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import './App.css';
 import BGsimple from "./assets/BGsimple.png";
 import puddle from "./assets/puddle.png";
 import {Portfolio} from "./pages/3-ProjectPage";
+import './components/threejs/three.js';
 
 function App() {
     return (
         <>
             <div>
                 <content>
-                    <img className="background-specs" src={BGsimple} alt="Water Background"/>
+                    {/*<img className="background-specs" src={BGsimple} alt="Water Background"/>*/}
                     <Portfolio className="content-specs"/>
-                    <img className="puddle-overlay" src={puddle} alt="Puddle Mask"/>
+                    {/*<img className="puddle-overlay" src={puddle} alt="Puddle Mask"/>*/}
                 </content>
             </div>
         </>
 
-    );
+);
 }
 
 export default App;
