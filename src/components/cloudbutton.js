5ad2bc021f6e9efb493f5255541fbafe8e0f9cdf
lotsa11 <49392443+lotsa11@users.noreply.github.com>
2022-01-14 16:19:12 +0100

    Fixes

--- src/components/cloudbutton.js
+++ src/components/cloudbutton.js
@@ -1,18 +1,18 @@
 import CloudIcon from "../assets/IconMajorBTN.svg";
 import '../pages/Overview.css';
-
+import { Link } from "react-router-dom";
 
 export function Cloudbutton (){
 
     return (
-
+        <Link to="/overview">
         <div id="majorButton">
-    <a href={"/1-Overview.js"}>
+
             <img className="icon" src={CloudIcon}/>
             <h2 class={"major"}>Major</h2>
-    </a>
-        </div>
 
+        </div>
+        </Link>
     );
 
 }
