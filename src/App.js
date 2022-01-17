0584fe5eb46abd458e9a796db6ee16f1187c6126
ColinSeanKavanagh <colin.sean.kavanagh@gmail.com>
2022-01-17 15:24:38 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -20,5 +20,13 @@ function App() {
 
 );
 }
+//Nicolas Search Bar
+// import Search from './search';
+//
+// const App = () => {
+//     return (
+//         <Search />
+//     );
+// }
 
 export default App;
