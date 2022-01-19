a64ce4ebbab8fdd9abd728b964d856a26b45e9c1
LeaSuLinHabel <lea.habel@yahoo.de>
2022-01-19 17:25:16 +0100

    Revert "Merge branch 'dev_Lin'"
    This reverts commit e3a76e7e04a4a0cd3426870d24d62eb929f5d144.

--- src/pages/1-Overview.js
+++ src/pages/1-Overview.js
@@ -1,35 +1,24 @@
 import React from "react";
-import '../../src/App.css';
-import { Person } from "../components/Person";
+import './Overview.css';
+import {Person} from "../components/Person";
 import CloudIcon from "../assets/cloudiIcon.png";
 import frame from "../assets/frame.png";
+import {Cloudbutton} from "../components/cloudbutton";
+import {CloseButton} from "../components/closeButton";
+import {Link} from "react-router-dom";
+import BGsimple from "../assets/BGsimple.png";
 
-import Img1 from '../assets/img1.png'
-
-export function Overview() {
+export function Overview (){
 
     return (
-        <>
-            <div className="component-display">
-                <div id="majorButton">
-                    <img className="icon" src={CloudIcon} />
-                    <h4>Send back to cloud</h4>
-                </div>
-                <Person
-                    name="Nicolas Saavedra-Rueda"
-                    major="Interaction Design"
-                    position="pos0"
-                    img1URL={Img1}
-                />
+        <div className="component-display">
+            <img className="background-specs" src={BGsimple} alt="Water Background"/>
+            <Cloudbutton/>
 
-                <Person
-                    name="Max Mustermann"
-                    major="Interaction Design"
-                    position="pos1"
-                    img1URL={Img1}
-                />
-            </div>
-        </>
+
+            <Person/>
+
+        </div>
     );
 
 }
