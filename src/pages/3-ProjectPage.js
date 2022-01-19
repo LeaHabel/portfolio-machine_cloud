d470315418315b71bd083065ab3f999250722576
CarinaSt <carina.steindl@edu.fh-joanneum.at>
2022-01-19 15:56:02 +0100

    error solved

--- src/pages/3-ProjectPage.js
+++ src/pages/3-ProjectPage.js
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 import './3-ProjectPage.css'
-import MaxMuster from "../assets/MaxMuster.png";
-import PortfolioBG from "../assets/PortfolioBG.png";
-import QRmm from "../assets/QRmm.png";
-import MAILicon from "../assets/MAILicon.png"
-import IGicon from "../assets/IGicon.png"
-import WEBicon from "../assets/WEBicon.png"
-import projIMG1 from "../assets/projIMG1.png"
-import projIMG2 from "../assets/projIMG2.png"
-import projIMG3 from "../assets/projIMG3.png"
-import Next from "../assets/Next.svg"
-import Previous from "../assets/Previous.svg"
-import IconMajorBTN from "../assets/IconMajorBTN.svg"
-import CloseBTN from "../assets/CloseBTN.svg"
-import data from "../assets/data/contactData.json"
+import MaxMuster from '../assets/MaxMuster.png';
+import PortfolioBG from '../assets/PortfolioBG.png';
+import QRmm from '../assets/QRmm.png';
+import MAILicon from '../assets/MAILicon.png';
+import IGicon from '../assets/IGicon.png';
+import WEBicon from '../assets/WEBicon.png';
+import projIMG1 from '../assets/projIMG1.png';
+import projIMG2 from '../assets/projIMG2.png';
+import projIMG3 from '../assets/projIMG3.png';
+import Next from '../assets/Next.svg';
+import Previous from '../assets/Previous.svg';
+import IconMajorBTN from '../assets/IconMajorBTN.svg';
+import CloseBTN from '../assets/CloseBTN.svg';
+import data from '../assets/data/personDataV2.json';
 
-export function Portfolio(props) {
+export function Portfolio() {
     /*    const data = [
             {id: 1, name: "John Doe"},
             {id: 2, name: "Victor Wayne"},
@@ -29,60 +29,43 @@ export function Portfolio(props) {
                 <img className="background-specs" src={PortfolioBG} alt="Portfolio Background"/>
                 <div className="contact">
                     <img className="profil" src={MaxMuster} alt="Profil"/>
-                    {data.PERSONAL_DETAILS.map((user) => (
-                            <div>{
-                                <>
-                                    <h1 className="head1">{user.Surname} {user.FirstName}</h1>
-                                    <p className="head3">{user.Major}</p>
-                                    <p className="paragraphw"><img className="icons" src={MAILicon}
-                                                                   alt="Mail"/>{user.Mail}
-                                    </p>
-                                    <p className="paragraphw"><img className="icons" src={IGicon}
-                                                                   alt="Instagram"/>{user.SocialMedia}</p>
-                                    <p className="paragraphw"><img className="icons" src={WEBicon}
-                                                                   alt="Website"/>{user.Website}</p>
-                                    <p className="head2">Skills</p>
-                                    <p className="paragraphw">{user.Skills}</p>
-                                    <img className="QR" src={QRmm} alt="QR Code"/>
-                                    <p className="paragraphw">Save Details</p>
-                                </>
-                            }
-                            </div>
-                        )
-                    )
-                    }
+                    <h1 className="head1">{data.PERSONAL_DETAILS[0].Surname} {data.PERSONAL_DETAILS[0].FirstName}</h1>
+                    <p className="head3">{data.PERSONAL_DETAILS[0].Major}</p>
+                    <p className="paragraphw"><img className="icons" src={MAILicon}
+                                                   alt="Mail"/>{data.PERSONAL_DETAILS[0].Mail}
+                    </p>
+                    <p className="paragraphw"><img className="icons" src={IGicon}
+                                                   alt="Instagram"/>{data.PERSONAL_DETAILS[0].SocialMedia}</p>
+                    <p className="paragraphw"><img className="icons" src={WEBicon}
+                                                   alt="Website"/>{data.PERSONAL_DETAILS[0].Website}</p>
+                    <p className="head2">Skills</p>
+                    <p className="paragraphw">{data.PERSONAL_DETAILS[0].Skills}</p>
+                    <img className="QR" src={QRmm} alt="QR Code"/>
+                    <p className="paragraphw">Save Details</p>
+
                 </div>
 
 
                 <div className="projectinfo">
-                    {data.PERSONAL_DETAILS.map((user) => (
-                            <div>{
-                                <>
-                                    <h1 className="head4">Project Title</h1>
-                                    <p className="head3">Communication Design</p>
-                                    <p className="paragraphb">Here is a short project description, consetetur sadipscing
-                                        elitr,
-                                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-                                        diam
-                                        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-                                        gubergren,
-                                        no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-                                        consetetur sadipscing elitr.</p>
-                                    <p className="paragraphbold">2021 | Sem X</p>
-                                    <p className="paragraphbold">Own role</p>
-                                    <p className="paragraphb">{user.ownrole_1}</p>
-                                    <p className="paragraphbold">Team</p>
-                                    <p className="paragraphb">Name Lastname, Name Lastname, Name Lastname</p>
-                                    <p className="arrows"><img className="arrows" src={Previous} alt="Previous"/><img
-                                        className="arrows"
-                                        src={Next}
-                                        alt="Next"/></p>
-                                </>
-                            }
-                            </div>
-                        )
-                    )
-                    }
+                    <h1 className="head4">Project Title</h1>
+                    <p className="head3">Communication Design</p>
+                    <p className="paragraphb">Here is a short project description, consetetur sadipscing
+                        elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+                        diam
+                        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+                        gubergren,
+                        no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                        consetetur sadipscing elitr.</p>
+                    <p className="paragraphbold">2021 | Sem X</p>
+                    <p className="paragraphbold">Own role</p>
+                    <p className="paragraphb">{data.PERSONAL_DETAILS[0].ownrole_1}</p>
+                    <p className="paragraphbold">Team</p>
+                    <p className="paragraphb">Name Lastname, Name Lastname, Name Lastname</p>
+                    <p className="arrows"><img className="arrows" src={Previous} alt="Previous"/><img
+                        className="arrows"
+                        src={Next}
+                        alt="Next"/></p>
                 </div>
 
                 <div>
